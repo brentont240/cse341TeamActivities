cd5f6f407adf2242bfa594fc480c3378f02a53bf
Brenton Trebilcock <tre15004@byui.edu>
2021-09-23 11:35:51 -0600

    Team activity week2

--- routes/ta02.js
+++ routes/ta02.js
@@ -2,14 +2,29 @@
 // Remember, you can make more of these placeholders yourself!
 const express = require('express');
 const router = express.Router();
-
+ 
+const names = ['peter', 'james', 'john'];
+ 
 router.get('/', (req, res, next) => {
-  res.render('pages', {
+  res.render('pages/ta02', {
     title: 'Team Activity 02',
     path: '/ta02', // For pug, EJS
-    activeTA03: true, // For HBS
-    contentCSS: true, // For HBS
+    users: names
   });
 });
+ 
+router.post('/addUser', (req, res, next) => {
+  names.push(req.body.username);
+  res.redirect('/ta02');
+});
 
+router.post('/removeUsername', (req, res, next) => {
+  for (let i = 0 ; i < names.length; i++){
+    if(names[i] == req.body.removeUsername )
+      names.splice(i,1);
+  };
+  res.redirect('/ta02');
+});
+
+ 
 module.exports = router;
